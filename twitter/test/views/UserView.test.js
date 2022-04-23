9b275ec13091a297b208733d4cc1f17129b02be0
EinarDvls <launchx06534@innovaccion.mx>
2022-04-23 02:16:06 -0500

    agregando el tercer requerimiento

--- twitter/test/views/UserView.test.js
+++ twitter/test/views/UserView.test.js
@@ -12,4 +12,11 @@ describe ("Test for UserView", () =>{
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/)
       })
+
+    test("3. Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+      })  
+
 })
